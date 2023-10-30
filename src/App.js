149892bf4c1f324e3c4517e90cda96fa1f987df3
akshanshdcode <akshansh.gupta13@gmail.com>
2023-10-30 19:08:15 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import AllRoutes from './Routes/AllRoutes';
 import Navbar from './Routes/Navbar';
+import HomeComp from './Components/HomeComp';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <HomeComp/>
       <AllRoutes/>
     </div>
   );
